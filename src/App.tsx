d988cb017699afd0e236da92270585ed85a361e6
amine-tayani <amine.tayani@gmail.com>
2024-07-26 20:58:50 +0100

    feat: add autocomplete component skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import { Autocomplete } from "./components/Autocomplete";
+import { facts } from "./data";
+
 function App() {
   return (
-    <div className="flex flex-col bg-neutral-900 items-center justify-center h-screen">
-      <h1 className="text-6xl font-bold text-neutral-200">MUI Autocomplete</h1>
+    <div className="flex flex-col pb-20 bg-neutral-900 items-center justify-center h-screen">
+      <h1 className="text-6xl font-bold text-neutral-200 mb-8">
+        Search for facts
+      </h1>
+      <Autocomplete
+        options={facts}
+        isOptionEqualToValue={(option, value) => option.label === value.label}
+      />
     </div>
   );
 }
---
+++ src/data.ts
@@ -0,0 +1,21 @@
+export const facts = [
+  { label: "Hummingbirds don't walk; they crawl." },
+  { label: "The world's largest land mammal is the African elephant." },
+  { label: "Bananas are actually berries." },
+  { label: "The average person laughs about 17 times a day." },
+  { label: "The strongest muscle in the human body is the jaw." },
+  { label: "A snail can sleep for three years." },
+  {
+    label:
+      "The total weight of all ants on Earth is equal to the total weight of all humans.",
+  },
+  { label: "The first computer mouse was made of wood." },
+  { label: "The longest word in the English language has 45 letters." },
+  { label: "A group of crows is called a murder." },
+  {
+    label: "The Sahara Desert is growing at a rate of about 10 miles per year.",
+  },
+  {
+    label: "The average person spends about 5 years of their life dreaming.",
+  },
+];
