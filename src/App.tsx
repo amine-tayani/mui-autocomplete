d6bfd5ffb375120dc38d99927d8fd13e647ea119
amine-tayani <amine.tayani@gmail.com>
2024-07-26 14:15:58 +0100

    chore: init project

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex flex-col bg-neutral-900 items-center justify-center h-screen">
+      <h1 className="text-6xl font-bold text-neutral-200">MUI Autocomplete</h1>
+    </div>
+  );
+}
+
+export default App;
